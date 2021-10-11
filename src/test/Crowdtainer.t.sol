b27a335093968519af22b2f184baa2ac3c77e66e
Thiago Faria Alencar <thiago@ti.eng.br>
2021-10-11 15:05:24 +0200

    Minor: cleanup

--- src/test/Crowdtainer.t.sol
+++ src/test/Crowdtainer.t.sol
@@ -6,28 +6,11 @@ import {Errors} from "../Crowdtainer.sol";
 
 contract CrowdtainerTester is CrowdtainerTest {
     function testDummyFunctionCalledByWrongAddress() public {
-        try bob.dummyFunction("I'm not allowed") {} catch Error(
-            string memory reason
-        ) {
-            // This is executed in case
-            // revert was called inside getData
-            // and a reason string was provided.
-            emit log("failed due reason: ");
-            emit log(reason);
-            return ();
-        } catch Panic(
-            uint256 /*errorCode*/
-        ) {
-            // This is executed in case of a panic,
-            // i.e. a serious error like division by zero
-            // or overflow. The error code can be used
-            // to determine the kind of error.
-            return ();
+        try bob.dummyFunction("I'm not allowed") {
+
         } catch (bytes memory lowLevelData) {
             string memory expectedError = "CallerNotAllowed(address,address)";
-
             assertEqSignature(expectedError, lowLevelData);
-
             printAddress2(lowLevelData);
         }
     }
