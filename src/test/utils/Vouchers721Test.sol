816a50d6be0b78fd5ee4292c57c43af7450b7044
Thiago Faria Alencar <thiago@ti.eng.br>
2022-01-19 17:49:34 +0100

    ----

--- src/test/utils/Vouchers721Test.sol
+++ src/test/utils/Vouchers721Test.sol
@@ -25,9 +25,9 @@ contract VoucherParticipant {
     address, 
     address, 
     uint256, 
-    bytes calldata
-    )external pure returns(bytes4) {
-        return bytes4(keccak256("onERC721Received(address,address,uint256,bytes)"));
+    bytes memory
+    ) external pure returns(bytes4) {
+        return this.onERC721Received.selector;
     } 
 
     function doJoin(
