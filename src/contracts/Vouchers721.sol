af1c7f662434b780b56bd52df7ad9c7b6a1d99ae
Someone <someone@somewhere.com>
2024-01-09 14:13:26 -0300

    missing change

--- src/contracts/Vouchers721.sol
+++ src/contracts/Vouchers721.sol
@@ -27,7 +27,6 @@ import "./Metadata/IMetadataService.sol";
  * @dev Each token id represents a "sold voucher", a set of one or more products or services of a specific Crowdtainer.
  */
 contract Vouchers721 is ERC721Enumerable {
-    using BitMaps for BitMaps.BitMap;
 
     // @dev Each Crowdtainer project is alloacted a range.
     // @dev This is used as a multiple to deduce the crowdtainer id from a given token id.
@@ -559,7 +558,7 @@ contract Vouchers721 is ERC721Enumerable {
     }
 
     function getClaimStatus(uint256 _tokenId) public view returns (bool) {
-        return claimed.get(_tokenId);
+        return BitMaps.get(claimed, _tokenId);
     }
 
     function setClaimStatus(uint256 _tokenId, bool _value) public {
